ea695cbd54deffd17e494fcf1b1cbc1cef37cd53
Ribya Ann Thomas <ribyaannthomas@RibyasMA.local>
2022-12-17 11:20:18 +0530

    input file

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+let fileReader;
+const handleFileRead = (e) => {
+    const content = fileReader.result;
+    console.log(content)
+    // … do something with the 'content' …
+  };
+const handleFileChosen = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
+function App() {
+  return (
+    <div className='upload-expense'>
+    <input
+      type='file'
+      id='file'
+      className='input-file'
+      accept='.csv'
+      onChange={e => handleFileChosen(e.target.files[0])}
+    />
+  </div>
+  );
+}
+
+export default App;
