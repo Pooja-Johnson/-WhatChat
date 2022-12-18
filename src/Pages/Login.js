189c1d37dcf9d6fe5e98589209be6550d0178cec
Pooja-Johnson <poojajohnson2002@gmail.com>
2022-12-18 08:32:00 +0530

    added Login

---
+++ src/Pages/Login.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "@firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1a4zIyfkPgRNSe7GsNvy4mk-HAtHLcUY",
+  authDomain: "whatchat-eb1e2.firebaseapp.com",
+  projectId: "whatchat-eb1e2",
+  storageBucket: "whatchat-eb1e2.appspot.com",
+  messagingSenderId: "600117262466",
+  appId: "1:600117262466:web:7421190983248b302d9c9b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+function Login() {
+
+  const navigate = useNavigate()
+  return (
+    <div className="App">
+      <button class="login-with-google-btn" onClick={signInWithGoogle=>{signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result)
+      navigate('/parse')
+    }).catch((error)=>{
+      alert(error.message)
+      })}
+    }>
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Parse.js
@@ -0,0 +1,77 @@
+
+import './App.css';
+import { useState } from 'react';
+import TransformText from './components/TransformText';
+//import { parseString } from 'whatsapp-chat-parser';
+
+function Parse() {
+  let fileReader;
+  const [content, setContent] = useState([])
+  const [active_user, setActive_user] = useState('')
+
+  const handleFileRead = (e) => {
+    const messages = fileReader.result.split(/\r?\n/);
+    setContent(messages)
+    //cons
+   // messages.forEach(message=>{
+      //console.log(message)
+      //const date_user=message.split(/\r?-/);
+      
+    };
+
+const handleFileChosen = (file) => {
+    fileReader = new FileReader();
+    //reader.addEventListener('loadend', txtLoadEndHandler);
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //setActive_user(e.target.value)
+    //console.log(active_user)
+  };
+  
+  
+  return (
+    <div className='bg'>
+       <form className='form'>
+          <div className='upload-expense '>
+              <label for="file"></label>
+              <input type='file'id='file'name='file' multiple
+                  onChange={e => handleFileChosen(e.target.files[0])}
+                />
+          </div>
+        <form  onSubmit={handleSubmit}>
+      
+      <br></br>
+              <button className='button'>Submit</button>
+        </form>
+        </form>
+           <input className='Name-text' placeholder='Type active user'
+            type='text'
+            name='user'
+            value={active_user}
+            onChange={e=>{setActive_user(e.target.value)
+            }}
+        
+
+           />
+      <div className='message-space'>
+      {content.map((mess)=>{
+        return(
+          <div >
+            <TransformText a_user={active_user} me={mess}/>
+          </div>
+        );
+         
+      })}
+    </div>
+   {/*<TransformText text={content[0]}/>*/}
+  
+  
+</div>
+);
+}
+
+export default Parse;
